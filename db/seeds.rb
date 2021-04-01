e418fab27f54aafbf3efe90444894b12bead44ec
mysta3 <myshawnestallings@gmail.com>
2021-03-31 17:24:59 -0700

    added messages to backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,3 @@ User.create(username: 'Itachi', password: 'test')
 User.create(username: 'Naruto', password: 'test')
 User.create(username: 'Vegeta', password: 'test')
 User.create(username: 'Gaara', password: 'test')
-
-Message.create(body: 'How can you defeat someone who has already seen the future?', user_id: 1)
-Message.create(body: 'I believe in my friends!', user_id: 2)
-Message.create(body: 'Im a Super Sayian', user_id: 3)
-Message.create(body: 'Im from the sand village!', user_id: 4)
